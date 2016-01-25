28c65a877049ad42cc6719d5f9a27a932611ef84
Lyubo <Lyubomir.Yonchev@gmail.com>
2016-01-25 09:58:12 +0200

    fixed bug with Newtonsoft Json, added countries and file upload

--- MasterChef/MasterChef.Data/Migrations/Configuration.cs
+++ MasterChef/MasterChef.Data/Migrations/Configuration.cs
@@ -19,96 +19,96 @@ namespace MasterChef.Data.Migrations
             this.AutomaticMigrationDataLossAllowed = true;
         }
 
-        //protected override void Seed(MasterChefDbContext context)
-        //{
-        //    if (context.Users.Any())
-        //    {
-        //        return;
-        //    }
+        protected override void Seed(MasterChefDbContext context)
+        {
+            if (context.Users.Any())
+            {
+                return;
+            }
 
-        //    var roleStore = new RoleStore<IdentityRole>(context);
-        //    var roleManager = new RoleManager<IdentityRole>(roleStore);
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
 
-        //    //Roles
-        //    var roles = Settings.Default.Roles.Split(',');
-        //    var adminRole = roles[0];
-        //    var userRole = roles[1];
-        //    foreach (var role in roles)
-        //    {
-        //        roleManager.Create(new IdentityRole { Name = role });
-        //    }
+            //Roles
+            var roles = Settings.Default.Roles.Split(',');
+            var adminRole = roles[0];
+            var userRole = roles[1];
+            foreach (var role in roles)
+            {
+                roleManager.Create(new IdentityRole { Name = role });
+            }
 
-        //    var userStore = new UserStore<AppUser>(context);
-        //    var userManager = new UserManager<AppUser>(userStore);
+            var userStore = new UserStore<AppUser>(context);
+            var userManager = new UserManager<AppUser>(userStore);
 
-        //    // Images
-        //    var defaultImage = new Image { Path = "~/Uploaded_Files/default.png" };
-        //    var adminImage = new Image { Path = "~/Uploaded_Files/admin.jpg" };
-        //    context.Images.AddOrUpdate(
-        //        defaultImage,
-        //        adminImage
-        //    );
+            // Images
+            var defaultImage = new Image { Path = "~/Uploaded_Files/default.png" };
+            var adminImage = new Image { Path = "~/Uploaded_Files/admin.jpg" };
+            context.Images.AddOrUpdate(
+                defaultImage,
+                adminImage
+            );
 
-        //    context.SaveChanges();
+            context.SaveChanges();
 
-        //    // Countries
-        //    var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\App_Data\countries.json");
-        //    var countries = JsonConvert.DeserializeObject<IEnumerable<Country>>(json);
+            // Countries
+            var json = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"\App_Data\countries.json");
+            var countries = JsonConvert.DeserializeObject<IEnumerable<Country>>(json);
 
-        //    foreach (var country in countries)
-        //    {
-        //        context.Countries.Add(country);
-        //    }
+            foreach (var country in countries)
+            {
+                context.Countries.Add(country);
+            }
 
-        //    // Users
-        //    var countryBulgaria = context.Countries.Local.Single(item => item.Name == "Bulgaria");
-        //    var userAdmin = new AppUser
-        //    {
-        //        UserName = "admin@admin",
-        //        Email = "admin@admin",
-        //        FirstName = NameData.GetFirstName(),
-        //        LastName = NameData.GetSurname(),
-        //        Country = countryBulgaria,
-        //        Image = adminImage
-        //    };
-        //    userManager.Create(userAdmin, userAdmin.UserName);
-        //    userManager.AddToRole(userAdmin.Id, adminRole);
+            //// Users
+            //var countryBulgaria = context.Countries.Local.Single(item => item.Name == "Bulgaria");
+            //var userAdmin = new AppUser
+            //{
+            //    UserName = "admin@admin",
+            //    Email = "admin@admin",
+            //    FirstName = NameData.GetFirstName(),
+            //    LastName = NameData.GetSurname(),
+            //    Country = countryBulgaria,
+            //    Image = adminImage
+            //};
+            //userManager.Create(userAdmin, userAdmin.UserName);
+            //userManager.AddToRole(userAdmin.Id, adminRole);
 
-        //    for (int userIndex = 0; userIndex < 100; userIndex++)
-        //    {
-        //        bool isSuccess = false;
+            //for (int userIndex = 0; userIndex < 100; userIndex++)
+            //{
+            //    bool isSuccess = false;
 
-        //        while (!isSuccess)
-        //        {
-        //            var firstName = NameData.GetFirstName();
-        //            var lastName = NameData.GetSurname();
-        //            var userName = firstName + "@" + lastName;
+            //    while (!isSuccess)
+            //    {
+            //        var firstName = NameData.GetFirstName();
+            //        var lastName = NameData.GetSurname();
+            //        var userName = firstName + "@" + lastName;
 
-        //            Country country = null;
-        //            while (country == null)
-        //            {
-        //                var countryName = PlaceData.GetCountry();
-        //                country = context.Countries.Local.SingleOrDefault(item => item.Name == countryName);
-        //            }
+            //        Country country = null;
+            //        while (country == null)
+            //        {
+            //            var countryName = PlaceData.GetCountry();
+            //            country = context.Countries.Local.SingleOrDefault(item => item.Name == countryName);
+            //        }
 
-        //            var user = new AppUser
-        //            {
-        //                UserName = userName,
-        //                Email = userName,
-        //                FirstName = firstName,
-        //                LastName = lastName,
-        //                Country = country,
-        //                Image = new Image() { Path = string.Format("~/Uploaded_Files/Users/{0}.jpg", userIndex + 1) }
-        //            };
-        //            isSuccess = userManager.Create(user, userName).Succeeded;
-        //            if (isSuccess)
-        //            {
-        //                userManager.AddToRole(user.Id, userRole);
-        //            }
-        //        }
-        //    }
+            //        var user = new AppUser
+            //        {
+            //            UserName = userName,
+            //            Email = userName,
+            //            FirstName = firstName,
+            //            LastName = lastName,
+            //            Country = country,
+            //            Image = new Image() { Path = string.Format("~/Uploaded_Files/Users/{0}.jpg", userIndex + 1) }
+            //        };
+            //        isSuccess = userManager.Create(user, userName).Succeeded;
+            //        if (isSuccess)
+            //        {
+            //            userManager.AddToRole(user.Id, userRole);
+            //        }
+            //    }
+            //}
 
-        //    context.SaveChanges();
-        //}
+            context.SaveChanges();
+        }
     }
 }
