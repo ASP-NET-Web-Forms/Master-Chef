da265cdcc774ef53445c7ea668ffa92b16ad2fa7
Lyubo <Lyubomir.Yonchev@gmail.com>
2016-01-25 14:39:57 +0200

    test

--- MasterChef/MasterChef.Models/AppUser.cs
+++ MasterChef/MasterChef.Models/AppUser.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
+
     using System.Collections.Generic;
     using System.Security.Claims;
     using System.Threading.Tasks;
